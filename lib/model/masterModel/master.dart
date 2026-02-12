1424e04831e3c4e085eb2ce80c243b84e99a395b
kakeven <kevsnsilba@gmail.com>
2026-02-12 11:39:24 -0300

    teste graduado

--- lib/model/masterModel/master.dart
+++ lib/model/masterModel/master.dart
@@ -1,3 +1,4 @@
+
 class Rolagens{
 //test para saber se a ação é efetuada
 bool testeSimples(int dado, int modificador,int bonus,int cd){
@@ -8,7 +9,24 @@ bool testeSimples(int dado, int modificador,int bonus,int cd){
   return false;
   }
 
-int testeGraduado(int cd,int dado,){
-  return 0;
+int testeGraduado(int cd,int dado,int bonus,int modificador){
+    int diferenca = (dado+bonus+modificador) - cd; //5
+    int grau;
+    if(dado==20) grau = 1;
+    
+    if(diferenca>=0){
+      grau = 1+(diferenca~/5);
+    }else {
+      grau = -1 + (diferenca~/5);
+    }
+    
+    if(dado==20) grau += 1;
+    if(dado==1)  grau -= 1;
+    grau = grau.clamp(-4, 4);     
+    return grau;
+  
+  
   }
+
+
 }
